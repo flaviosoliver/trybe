0dee68a2537710342f9ed77c985844adbc00c85b
Flavio Oliveira <flavsoliver@gmail.com>
2020-07-16 15:54:40 -0300

    arquivo script_ini.js #Objetos JS

---
+++ javascript/exercises/4_4/script_ini.js
@@ -0,0 +1,16 @@
+// Objetos
+
+let carro = {
+  placa: 'AAA-0000',
+  marca: 'Fiat',
+  modelo: 'Grand Siena',
+  opcionais: {
+    cambio: 'manual',
+    combustivel: 'felx'
+  },
+  "kit eletrico": "alarme, travas e vidros"
+}
+
+console.log(carro.placa)
+
+console.log(carro["kit eletrico"])
